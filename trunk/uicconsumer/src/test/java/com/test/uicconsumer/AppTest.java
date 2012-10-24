f38ee251190904ab07916d6b6f280a8217ea3769
hwy1782@gmail.com <hwy1782@gmail.com@fce019bf-aca0-cb44-73dd-7cdc73da1c61>
2012-10-24 08:38:42 +0000

    uic hsf

--- trunk/uicconsumer/src/test/java/com/test/uicconsumer/AppTest.java
+++ trunk/uicconsumer/src/test/java/com/test/uicconsumer/AppTest.java
@@ -12,13 +12,11 @@ public class AppTest {
 
 	@Test
 	public void testApp() {
-		HSFEasyStarter.start("d:/hsf/release", "");//在用到consumer bean前启动hsf
+		HSFEasyStarter.start("d:/hsf/release", "");// 在用到consumer bean前启动hsf
 		String springResourcePath = "spring-hsf-uic-consumer.xml";
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
-				springResourcePath);
-		UicReadService uicReadService = (UicReadService) ctx
-				.getBean("uicReadService");
-		ServiceUtil.waitServiceReady(uicReadService);//不是必须，因为运行很快，所以要在configServer推地址下来前检测地址是否推下來，可以自己sleep(500)
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(springResourcePath);
+		UicReadService uicReadService = (UicReadService) ctx	.getBean("uicReadService");
+		ServiceUtil.waitServiceReady(uicReadService);// 不是必须，因为运行很快，所以要在configServer推地址下来前检测地址是否推下來，可以自己sleep(500)
 		BaseUserDO user = uicReadService.getBaseUserByUserId(10000L, "detail")
 				.getModule();
 		System.out.println("user[id:10000L] nick:" + user.getNick());
